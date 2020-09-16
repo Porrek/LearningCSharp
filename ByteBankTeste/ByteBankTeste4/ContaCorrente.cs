d8d096e1d4b5504909567067e65bb1a827c02df0
Fábio Porreca <fmporreca@gmail.com>
2020-09-15 22:45:39 -0300

    Aula 05 ALURA

---
+++ ByteBankTeste/ByteBankTeste4/ContaCorrente.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankTeste4
+{
+    public class ContaCorrente
+    {
+        private Cliente _titular;
+        public Cliente Titular { get; set; }
+
+        public int Agencia { get; set; }
+        public int Numero { get; set; }
+
+        private double _saldo = 100;
+        /* Todos os Objetos privados precisam ser setados com underline na frente.         * 
+         * O método utilizado para ser possível utilizar o saldo, que está como 'private'
+         * é chamado de encapsulamento.
+         */
+
+
+        public double Saldo 
+        {
+            get
+            {
+                return _saldo
+            }
+            set
+            {
+                if (value < 0)
+            }
+
+        //public void DefinirSaldo(double saldo)
+        //{
+        //    if (saldo < 0)
+        //    {
+        //        return;
+        //    }
+
+        //    this.saldo = saldo; // 'this.' serve p/ referencia para o objeto que está chamando.
+        //}
+
+        //public double ObterSaldo()
+        //{
+        //    return saldo;
+        //}
+
+        public bool Sacar(double valor)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }            
+            _saldo -= valor;
+            return true;
+        }
+
+        public void Depositar(double valor)
+        {
+            this._saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this._saldo < valor)
+            {
+                return false;
+            }
+            this._saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+        }
+    }
+}
---
+++ ByteBankTeste/ByteBankTeste4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankTeste4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.Saldo = -10;
+
+            Console.WriteLine(conta.Saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
