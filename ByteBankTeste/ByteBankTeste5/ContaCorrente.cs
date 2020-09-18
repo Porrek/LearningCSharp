f54b28f2b494ab96d7d1c89b6c804564cd54d899
Fábio Porreca <fmporreca@gmail.com>
2020-09-17 22:41:08 -0300

    Aula 06 ALURA

---
+++ ByteBankTeste/ByteBankTeste5/ContaCorrente.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankTeste5
+{
+    public class ContaCorrente
+    {
+        private Cliente _titular;
+        public Cliente Titular { get; set; }
+        public static int TotalDeContasCriadas { get; private set; }
+        /* static é uma propriedade que pertence a classe, 
+         * todos os objetos compartilham essa informação.
+         * O set estando como 'private' não deixa com que se mude o valor das propriedades.
+         */
+        public int Agencia { get; set; }
+        public int Numero { get; set; }
+
+        private double _saldo = 100;
+        public double Saldo { get; set; }
+
+
+        /* O Construtor abaixo gera uma dependencia, pois agora fica prioritário apontar os valores
+         * para as variáveis 'agencia' e 'numero' quando se instancia a classe ContaCorrente.
+         */
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+            TotalDeContasCriadas++;
+        }
+
+        public ContaCorrente()
+        {
+
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }            
+            _saldo -= valor;
+            return true;
+        }
+
+        public void Depositar(double valor)
+        {
+            this._saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this._saldo < valor)
+            {
+                return false;
+            }
+            this._saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+        }
+    }
+}
---
+++ ByteBankTeste/ByteBankTeste5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankTeste5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(867, 86712540);
+            Console.WriteLine("Total de Contas Criadas: ",ContaCorrente.TotalDeContasCriadas);
+
+            Console.WriteLine(conta.Agencia);
+            Console.WriteLine(conta.Numero);
+
+            ContaCorrente contaDaGabriela = new ContaCorrente(847, 8672820);
+
+            Console.WriteLine(conta.Agencia);
+            Console.WriteLine(conta.Numero);
+
+            Console.WriteLine("Total de Contas Criadas: ", ContaCorrente.TotalDeContasCriadas);
+        }
+    }
+}
