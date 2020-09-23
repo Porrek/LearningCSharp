7ed44e9021b5c94accda86772e81c3e8b36cb4a1
Fábio Porreca <fmporreca@gmail.com>
2020-09-22 22:41:34 -0300

    Aula 03 - 01 - Alura

---
+++ ByteBankTeste/ByteBankTeste6/Funcionarios/Funcionario.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankTeste6.Funcionarios
+{
+    public class Funcionario
+    {
+        /* 0 - funcionpario não definido
+         * 1 - Diretor
+         * 2 - Designer
+         * 3 - Gerente de conta corrente
+         * N - ...
+        */ 
+        private int _tipo;
+
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario(int tipo)
+        {
+            _tipo = tipo;
+        }
+
+
+        public double ObterBonificacao()
+        {
+            if (_tipo == 1)
+            {
+                return Salario;
+            }
+            return Salario * 0.10;
+        }
+
+    }
+}
---
+++ ByteBankTeste/ByteBankTeste6/Program.cs
@@ -0,0 +1,24 @@
+﻿using ByteBankTeste6.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankTeste6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario carlos = new Funcionario(1);
+
+            carlos.Nome = "Carlos";
+            carlos.CPF = "546.879.157-20";
+            carlos.Salario = 2000;
+
+            Console.WriteLine("O {0} portador do CPF {1}, tem salário igual a: R$ {2}",carlos.Nome, carlos.CPF, carlos.Salario);
+
+        }
+    }
+}
