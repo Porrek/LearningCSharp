a5877acbbb24f5c977eaa60127ada452afeac270
Fábio Porreca <fmporreca@gmail.com>
2020-09-30 22:12:38 -0300

    Aula 04 - Módulo 01 - Alura

---
+++ ByteBankExcecoes/ByteBankExcecoes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankExcecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(86712345, 867);
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+            ContaCorrente contaDaGabriela = new ContaCorrente(987441555, 987);
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+            Console.ReadLine();
+        }
+    }
+}
